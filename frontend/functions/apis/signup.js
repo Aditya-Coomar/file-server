014817393347c0655de8dc19b015a1030b54a588
Aditya-Coomar <adi.coomar04@gmail.com>
2024-11-04 17:17:48 +0530

    feat: add otp sending api

--- frontend/functions/apis/signup.js
+++ frontend/functions/apis/signup.js
@@ -30,4 +30,31 @@ const UserSignup = async (email, fullname, username, password) => {
   }
 };
 
-export { UserSignup };
+const SendVerificationEmail = async (email) => {
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  const raw = JSON.stringify({
+    email: email,
+  });
+
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+
+  try {
+    const response = await fetch(
+      `${SERVER_URL}/api/auth/otp/send/account`,
+      requestOptions
+    );
+    const result = await response.text();
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export { UserSignup, SendVerificationEmail };
